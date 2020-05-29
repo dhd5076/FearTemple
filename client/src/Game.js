9fa6fe9dbd3062be7d746e432b54e7723fc4a454
Dylan Dunn <robodylan1@gmail.com>
2020-05-28 22:05:08 -0400

    Updated Text and Made Images Client Side

--- client/src/Game.js
+++ client/src/Game.js
@@ -5,13 +5,16 @@ import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Badge from 'react-bootstrap/Badge';
-import Button from 'react-bootstrap/Badge';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
 
 class Game extends React.Component {
   render() {
     return(
-      <Container className="p-4 m-2 bg-dark col-12 text-info">
-        <span class="justify-content-between d-flex">
+      <Container  className="gbg p-4 m-2 bg-light col-12 text-info">
+        <span className="justify-content-between d-flex">
           <h1 className="text-white mb-4"> Temple Of Fear</h1>
           <h1 className="text-white mb-4"> Round 1</h1>
         </span>
@@ -26,11 +29,49 @@ class Game extends React.Component {
             <button type="button" className="mt-2 btn btn-primary"> Toggle Music</button> { /* Because the bootstrap-react one is broken */ }
           </div>
         </Row>
+      <LoadModal />
       </Container> 
     )
   }
 }
 
+//Handles showing and dismissing the join game modal
+function LoadModal(props) {
+
+  const [show, setShow] = React.useState(true); //Sorta Mess
+
+  const closeThis = () => setShow(false); //Mess
+
+  const handleClose = function() {
+    closeThis(); //Bigger Mess
+  }
+
+  return (
+    <Modal
+      {...props}
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      show={show}
+    >
+      <Modal.Body>
+        <h4>Join Game</h4>
+        <InputGroup className="mt-4">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl className="col-12"
+            placeholder="Username"
+          />
+        </InputGroup>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button id="joinGameButton" onClick={handleClose}>Join Game</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+//The cards in the players hand
 class PlayerHand extends React.Component {
   render() {
     return(
@@ -46,6 +87,7 @@ class PlayerHand extends React.Component {
   }
 }
 
+//The set of cards that have already been played
 class CurrentRound extends React.Component {
   render() {
     return(
@@ -70,7 +112,7 @@ class GameCard extends React.Component {
         <Card.ImgOverlay>
           <Card.Title className="text-white"> 
             {this.props.value ? (
-              <Badge variant="success" className="rounded-circle" style={{height: '40px', width: '40px'}}> 
+              <Badge variant="secondary" className="rounded-circle" style={{height: '40px', width: '40px'}}> 
               <h4> {this.props.value} </h4>
               </Badge>
             ) : (
@@ -89,6 +131,7 @@ class GameCard extends React.Component {
   }
 }
 
+//Shows The List of Players
 class PlayersList extends React.Component {
   render() {
     return(
@@ -101,6 +144,7 @@ class PlayersList extends React.Component {
   }
 }
 
+//A single player element in the list of players
 class Player extends React.Component {
   constructor(props) {
     super(props)
